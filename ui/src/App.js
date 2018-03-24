ee2c025c043131de2225cca88713b91341a4165b
Christopher Henn <chris@chrishenn.net>
2018-03-24 12:32:18 -0700

    Show Graph name if available

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Graph from "./Graph";
 
 const GRAPH_FIXTURE = {
+  name: "My Graph",
   nodes: [{ id: "a", label: "a" }, { id: "b" }, { id: "c" }],
   edges: [{ source: "a", target: "b" }, { source: "b", target: "c" }]
 };
@@ -11,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {GRAPH_FIXTURE.name && <h1>{GRAPH_FIXTURE.name}</h1>}
         <Graph nodes={GRAPH_FIXTURE.nodes} edges={GRAPH_FIXTURE.edges} />
       </div>
     );
