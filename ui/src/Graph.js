d57a3729922eaa2a2aa52f26524e525a320f9323
Christopher Henn <chris@chrishenn.net>
2018-03-26 12:05:11 -0700

    Use SVG instead of canvas to render graph

--- ui/src/Graph.js
+++ ui/src/Graph.js
@@ -3,18 +3,23 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 import debounce from "lodash/debounce";
-import { setCanvasDimensions } from "./utils";
 
 const NODE_PADDING = 4;
 const NODE_DISTANCE = 30;
 const NODE_FILL = "#dfe6e9";
 const NODE_STROKE = "#636e72";
-const NODE_STROKE_WIDTH = 2;
+const NODE_STROKE_WIDTH = 1;
+const NODE_TEXT_FILL = "#2d3436";
 const EDGE_STROKE = "#636e72";
-const EDGE_STROKE_WIDTH = 2;
-const NODE_LABEL_FILL = "#2d3436";
-const NODE_LABEL_FONT = "bold 12px Helvetica, serif";
-const NODE_LABEL_HEIGHT = 12;
+const EDGE_STROKE_WIDTH = 1;
+
+const truncate = (text, k = 5) => {
+  if (text.length < k + 2) {
+    return text;
+  }
+
+  return `${text.slice(0, k)}...`;
+};
 
 class Graph extends Component {
   windowDidResize = debounce(this.renderD3.bind(this), 100);
@@ -35,24 +40,102 @@ class Graph extends Component {
   render() {
     return (
       <div className="Graph">
-        <canvas ref={el => (this.canvas = el)} />
+        <svg ref={el => (this.svg = el)} />
       </div>
     );
   }
 
   renderD3() {
-    if (!this.canvas) {
+    if (!this.svg) {
       throw new Error("No element to render into");
     }
 
     const { nodes, edges } = this.props;
-    const width = this.canvas.parentElement.offsetWidth;
-    const height = this.canvas.parentElement.offsetHeight;
-    const context = this.canvas.getContext("2d");
+    const width = this.svg.parentElement.offsetWidth;
+    const height = this.svg.parentElement.offsetHeight;
+    const svg = d3.select(this.svg);
+
+    svg.attr("width", width).attr("height", height);
+
+    const edgeJoin = svg.selectAll("g.edge").data(edges);
+
+    edgeJoin
+      .enter()
+      .append("g")
+      .classed("edge", true)
+      .append("line")
+      .attr("stroke", EDGE_STROKE)
+      .attr("stroke-width", EDGE_STROKE_WIDTH);
+
+    edgeJoin.exit().remove();
+
+    const nodeJoin = svg.selectAll("g.node").data(nodes);
+
+    const nodeJoinEnter = nodeJoin
+      .enter()
+      .append("g")
+      .classed("node", true);
+
+    nodeJoinEnter
+      .append("rect")
+      .attr("stroke", NODE_STROKE)
+      .attr("stroke-width", NODE_STROKE_WIDTH)
+      .attr("fill", NODE_FILL);
+
+    nodeJoinEnter
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("alignment-baseline", "middle")
+      .attr("font-family", "Helvetica, sans-serif")
+      .attr("font-size", "12px")
+      .attr("font-weight", "bold")
+      .attr("fill", NODE_TEXT_FILL)
+      .text(d => truncate(d.name || d.id))
+      .each(function(d) {
+        const { width, height } = this.getBBox();
+
+        // Capture width and height of text node for future calculations.
+        // Mutating the datum for a selection is wack af, but it's already
+        // done quite liberally by d3-force ¯\_(ツ)_/¯
+        d.width = width;
+        d.height = height;
+      });
+
+    // Now we can update the rects to accomodate the correct text size
+    nodeJoinEnter
+      .select("rect")
+      .attr("width", d => d.width + NODE_PADDING * 2)
+      .attr("height", d => d.height + NODE_PADDING * 2);
+
+    nodeJoin.exit().remove();
+
+    const updateEdges = () => {
+      svg
+        .selectAll("line")
+        .attr("x1", d => d.source.x)
+        .attr("x2", d => d.target.x)
+        .attr("y1", d => d.source.y)
+        .attr("y2", d => d.target.y);
+    };
+
+    const updateNodes = () => {
+      svg
+        .selectAll("g.node")
+        .select("rect")
+        .attr("x", d => d.x - d.width / 2 - NODE_PADDING)
+        .attr("y", d => d.y - d.height - NODE_PADDING / 2);
+
+      svg
+        .selectAll("text")
+        .attr("x", d => d.x)
+        .attr("y", d => d.y);
+    };
+
     const edgeForce = d3
       .forceLink()
       .id(d => d.id)
       .distance(NODE_DISTANCE);
+
     const simulation = d3
       .forceSimulation()
       .force("edge", edgeForce)
@@ -60,49 +143,9 @@ class Graph extends Component {
       .force("center", d3.forceCenter(width / 2, height / 2));
 
     const onTick = () => {
-      context.clearRect(0, 0, width, height);
-
-      // Draw edges
-      context.beginPath();
-      edges.forEach(edge => {
-        context.moveTo(edge.source.x, edge.source.y);
-        context.lineTo(edge.target.x, edge.target.y);
-      });
-      context.lineWidth = EDGE_STROKE_WIDTH;
-      context.strokeStyle = EDGE_STROKE;
-      context.stroke();
-
-      // Draw nodes
-      context.beginPath();
-
-      context.font = NODE_LABEL_FONT;
-      context.textAlign = "center";
-      context.textBaseline = "middle";
-      context.lineWidth = NODE_STROKE_WIDTH;
-      context.strokeStyle = NODE_STROKE;
-
-      nodes.forEach(node => {
-        const label = node.name || node.id;
-        const { width } = context.measureText(label);
-
-        const rect = [
-          node.x - width / 2 - NODE_PADDING,
-          node.y - NODE_LABEL_HEIGHT / 2 - NODE_PADDING,
-          width + 2 * NODE_PADDING,
-          NODE_LABEL_HEIGHT + 2 * NODE_PADDING
-        ];
-
-        context.fillStyle = NODE_FILL;
-        context.fillRect(...rect);
-        context.strokeRect(...rect);
-
-        context.fillStyle = NODE_LABEL_FILL;
-        context.fillText(label, node.x, node.y);
-        context.fill();
-      });
+      updateEdges();
+      updateNodes();
     };
-
-    setCanvasDimensions(this.canvas, width, height);
 
     simulation.nodes(nodes).on("tick", onTick);
     simulation.force("edge").links(edges);
